7ba2e2407c3175050729ec397ace35d9120626aa
gribVolnushka <burgerqueen98@gmail.com>
2024-08-27 15:57:57 +0200

    changed types of variable

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-let userName = prompt("What's your name?");
-let userAge = prompt("How old are you?");
-let welcomeMessage = `Hello ${userName}, you are ${userAge} years old.`;
+const userName = prompt("What's your name?");
+const userAge = prompt('How old are you?');
+const welcomeMessage = `Hello ${userName}, you are ${userAge} years old.`;
 
 alert(welcomeMessage);
 
-let ageInFiveYears = Number(userAge) + 5;
+const ageInFiveYears = Number(userAge) + 5;
 
 alert(`In five years, you will be ${ageInFiveYears}.`);
